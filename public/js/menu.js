b5b5bee07f77b4761e8597eff7d7288b60124376
Leandro Luna <135192663+LeandroLuna22@users.noreply.github.com>
2025-11-26 14:13:13 -0300

    Restore Update
    Correção de arquivos

---
+++ public/js/menu.js
@@ -0,0 +1,41 @@
+fetch('/user')
+  .then(res => res.json())
+  .then(user => {
+    
+    const sidebar = document.getElementById('sidebar');
+
+    let links = `
+      <div class="logo">SISGO</div>
+      
+      <a class="menu-btn" href="/visualizar-notas.html">
+        <span>📄</span> Visualizar Notas
+      </a>
+
+      <a class="menu-btn" href="/visualizar-ordens.html">
+        <span>📋</span> Visualizar Ordens
+      </a>
+    `;
+
+    if (user.tipo === 'admin') {
+      links =
+        `
+        <div class="logo">SISGO (ADM)</div>
+
+        <a class="menu-btn" href="/criar-nota.html">
+          <span>➕</span> Criar Nota
+        </a>
+
+        <a class="menu-btn" href="/cadastro.html">
+          <span>👤</span> Cadastrar Usuário
+        </a>
+        ` + links;
+    }
+
+    links += `
+      <a class="menu-btn logout" href="/logout">
+        <span>🚪</span> Sair
+      </a>
+    `;
+
+    sidebar.innerHTML = links;
+  });
